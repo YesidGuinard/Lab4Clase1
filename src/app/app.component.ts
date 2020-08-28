d23d9a35a6425a51e962ff75c4022b358062d883
Yesid Guinard <ydguinards@gmail.com>
2020-08-27 21:42:39 -0300

    Creacion de Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+
+public ejemplo = 'Hola Mundo';
+public ejemplo2 = 'Hola Mundo';
+constructor() { }
+
+ngOnInit() {
 }
+
+PruebaClick() {
+  this.ejemplo2 = 'Ejecutamos el evento!!!';
+}
+
+}
+
